323071c7ff8eea2115b6109c94da453e53ff1fc6
Cruglet <cruglet@gmail.com>
2025-01-03 17:00:53 -0500

    Ensure projectlist file exists on startup

--- menus/init/main_window.gd
+++ menus/init/main_window.gd
@@ -7,8 +7,14 @@ const MIN_WINDOW_SIZE: Vector2i = Vector2i(760, 500)
 
 func _ready() -> void:
 	Singleton.main_window = self
+	_ensure_file_integrity()
 	set_window_config()
 	add_child(MAIN_MENU.instantiate())
+
+func _ensure_file_integrity() -> void:
+	if !FileAccess.file_exists("res://projectlist.json"):
+		var file: FileAccess = FileAccess.open("res://projectlist.json", FileAccess.WRITE)
+		file.close()
 
 func set_window_config() -> void:
 	DisplayServer.window_set_min_size(MIN_WINDOW_SIZE)
