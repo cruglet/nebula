a8ca664400675c79f0ef025ed30ed4d572920515
Cruglet <cruglet@gmail.com>
2025-01-11 20:49:46 -0500

    Finalize transition to Rust level dumping
    Alright, the level AND its subareas are loaded through rust now. All that's missing is to continue where I left off with the rest of the level file info: camera profiles, paths, regions, and tiles.

--- menus/main_menu/home/home.gd
+++ menus/main_menu/home/home.gd
@@ -17,9 +17,7 @@ func _debug() -> void:
 	projects_container.visible = false
 	
 	var lvl: NSMBWLevel = NSMBWLevel.new()
-	lvl.dump_level("01-02.arc", "test/", "test/")
-	await lvl.dump_finished
-	lvl.read_level("test/course/")
+	lvl.dump_level("01-02", "test/", "test/")
 
 func _ready() -> void:
 	# Determine if there are projects available or not.
