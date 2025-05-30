2f6374a84320be485caac15f837cebc21afea5e6
cruglet <cruglet@gmail.com>
2025-05-30 09:20:23 -0400

    Prevent duplicate level opening

--- modules/nsmbw/editor/open_level.gd
+++ modules/nsmbw/editor/open_level.gd
@@ -21,20 +21,34 @@ func show_open_level_dialog() -> void:
 
 
 func parse_level(path: String) -> void:
-	var arc: ARC = ARC.open(path)
-	var level: NSMBWLevel = NSMBWLevel.open(arc)
-	var level_canvas: Node = LEVEL_CANVAS.duplicate(true).instantiate()
 	
-	level_canvas.level = level
-	level_canvas.name = path.get_file().replace(".", ",")
+	var level_opened: bool = false
+	var file_name: String = path.get_file()
+	var level_index: int = 0
 	
-	viewport_tab_container.add_child(level_canvas)
+	for i: int in range(viewport_tab_container.get_tab_count()):
+		if viewport_tab_container.get_tab_title(i) == file_name:
+			level_opened = true
+			level_index = i
+			break
 	
-	if viewport_tab_container.get_child(0).name == "<empty>":
-		viewport_tab_container.remove_child(viewport_tab_container.get_child(0))
-	
-	var tab_bar: TabBar = viewport_tab_container.get_tab_bar()
-	tab_bar.set_tab_icon(tab_bar.tab_count - 1, LEVEL_ICON)
-	tab_bar.set_tab_title(tab_bar.tab_count - 1, level_canvas.name.replace(",", "."))
-	
-	viewport_tab_container.current_tab = tab_bar.tab_count - 1
+	if not level_opened:
+		var arc: ARC = ARC.open(path)
+		var level: NSMBWLevel = NSMBWLevel.open(arc)
+		var level_canvas: Node = LEVEL_CANVAS.duplicate(true).instantiate()
+		
+		level_canvas.level = level
+		level_canvas.name = path.get_file().replace(".", ",")
+		
+		viewport_tab_container.add_child(level_canvas)
+		
+		if viewport_tab_container.get_child(0).name == "<empty>":
+			viewport_tab_container.remove_child(viewport_tab_container.get_child(0))
+		
+		var tab_bar: TabBar = viewport_tab_container.get_tab_bar()
+		tab_bar.set_tab_icon(tab_bar.tab_count - 1, LEVEL_ICON)
+		tab_bar.set_tab_title(tab_bar.tab_count - 1, level_canvas.name.replace(",", "."))
+		
+		viewport_tab_container.current_tab = tab_bar.tab_count - 1
+	else:
+		viewport_tab_container.current_tab = level_index
