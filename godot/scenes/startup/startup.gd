a6f617b5e547ec781cd35ab12db10d2432f6b900
cruglet <cruglet@gmail.com>
2025-08-08 16:35:21 -0400

    Core: Startup scene

---
+++ godot/scenes/startup/startup.gd
@@ -0,0 +1,42 @@
+extends Control
+
+const SPLASH_MESSAGES: Array[String] = [
+	"Loading...",
+	"Getting things ready...",
+	"Boot sequence initiated...",
+	"Finding star coins...",
+	"Preparing warp pipes...",
+	"Consuming star bits...",
+	"Hoarding 1-UPs...",
+	"Calibrating warp zones...",
+	"Aligning launch stars...",
+	"Scanning for hidden blocks...",
+	"Preparing cannons...",
+	"Gathering Miis...",
+	"Collecting red coins...",
+	"Launching into space...",
+	"Fighting with the camera...",
+	"Winning by doing absolutely nothing...",
+	"Breaking blocks...",
+	"Placing blocks...",
+	"Questioning blocks...",
+	"Kicking shells...",
+	"Debugging for hours...",
+]
+
+@export var animation_player: AnimationPlayer
+@export var splash_texture: TextureRect
+@export var splash_label: Label
+
+
+func _ready() -> void:
+	splash_label.text = SPLASH_MESSAGES.pick_random()
+	animation_player.play(&"startup")
+
+
+func _after_startup_animation() -> void:
+	animation_player.play(&"startup_finished")
+
+
+func _after_startup_finished_animation() -> void:
+	pass
